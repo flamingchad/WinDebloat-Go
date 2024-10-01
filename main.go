4d3385f24262e617a0f160641156321e95062ab4
Nithiesh Mohan <nithiesh164@gmail.com>
2024-10-01 21:47:06 +0530

    Added disableWeatherAndNews, removeBing, disableCopilot

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 func main() {
-	//disableDiagTrack()
-	//disableTelemetry()
-	//disableWeatherAndNews()
-	//removeBing()
+	disableDiagTrack()
+	disableTelemetry()
+	disableWeatherAndNews()
+	removeBing()
 	disableCopilot()
 }
